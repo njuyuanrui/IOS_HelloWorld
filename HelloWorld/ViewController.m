484de99246ff30e11cfa836860bb2e78ec3ca29b
Rui Yuan <ruiyuan@wenbo.local>
2015-01-20 00:36:59 +0800

    做一个输入文本并输出的Helloworld，现在遇到一些问题，总在获取textfield的文本处崩溃..

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -10,7 +10,18 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+
+- (IBAction)changeGreeting:(id)sender;
+
+
 @end
+
+
 
 @implementation ViewController
 
@@ -23,5 +34,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)changeGreeting:(id)sender {
+    
+    
+    self.userName = self.textField.text;
+    
+    NSString *nameString = self.userName;
+    if(nameString.length==0){
+        nameString = @"World";
+    
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello,%@!",nameString ];
+    
+    self.label.text=greeting;
+    
+}
+
+- (BOOL) textFieldShouldReturn: (UITextField *) theTextField{
+    if(theTextField == self.textField){
+        [theTextField resignFirstResponder];
+    }
+    return YES;
+}
+
 
 @end
